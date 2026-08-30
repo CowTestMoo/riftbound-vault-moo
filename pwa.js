9a526d6a624d046cbc186b79448ae5677ced2786
CowTestMoo <matthewxp100@hotmail.com>
2026-08-30 06:44:38 -0400

    Register PWA cache earlier on Safari

--- pwa.js
+++ pwa.js
@@ -3,10 +3,8 @@
 
   if(!('serviceWorker' in navigator) || !window.isSecureContext)return;
 
-  window.addEventListener('load',()=>{
-    navigator.serviceWorker
-      .register('./sw.js',{scope:'./',updateViaCache:'none'})
-      .then(registration=>registration.update().catch(()=>{}))
-      .catch(()=>{});
-  },{once:true});
+  navigator.serviceWorker
+    .register('./sw.js',{scope:'./',updateViaCache:'none'})
+    .then(registration=>registration.update().catch(()=>{}))
+    .catch(()=>{});
 })();
